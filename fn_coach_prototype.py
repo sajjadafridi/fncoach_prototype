f29d624766937f4854d45e27c00246f6a6e23e0e
lil_blizzard <plp248@nyu.edu>
2019-09-15 17:13:02 -0400

    scraping weapon and location data locations contains duplicate and old values

--- fn_coach_prototype.py
+++ fn_coach_prototype.py
@@ -1,13 +1,40 @@
 import PySimpleGUI as sg
 import sqlite3
+import requests
+from bs4 import BeautifulSoup
+
+### SCRAPING ###
+
+def update_weapon_list():
+    weapons_url = 'https://db.fortnitetracker.com/weapons'
+    weapons_page = requests.get(weapons_url)
+    soup = BeautifulSoup(weapons_page.content, 'html.parser')
+    weapons = []
+    for weapon in soup.find_all('h3', class_="trn-card__header-title"):
+        weapon_title = weapon.text
+        weapons.append(weapon_title)
+    return weapons
+
+def update_locations_list():
+    locations_url = 'https://fortnite.fandom.com/wiki/Locations_(Battle_Royale)'
+    locations_page = requests.get(locations_url)
+    soup = BeautifulSoup(locations_page.content, 'html.parser')
+    locations = []
+    for page_content in soup.find_all('div', class_='mw-content-ltr mw-content-text'):
+        for location_list_unordered in page_content.find_all('ul'):
+            for location_a in location_list_unordered.find_all('a'):
+                if location_a != None:
+                    locations.append(location_a.text)
+    set(locations)
+    return locations
 
 ### GUI ###
 
 data_entry_layout = [
     [sg.Text('FNCoach')],
-    [sg.Text('Where did you die?', size=(21,1)), sg.InputText(key='_DEATH_LOCATION_')],
-    [sg.Text('What weapon did you die to?', size=(21,1)), sg.InputText(key='_DEATH_WEAPON_')],
-    [sg.Text('What placement did you get?', size=(21,1)), sg.InputText(key='_PLACEMENT_')],
+    [sg.Text('Where did you die?', size=(21,1)), sg.Combo(update_locations_list(), key='_DEATH_LOCATION_')],
+    [sg.Text('What weapon did you die to?', size=(21,1)), sg.Combo(update_weapon_list(), key='_DEATH_WEAPON_')],
+    [sg.Text('What placement did you get?', size=(21,1)), sg.Combo([i for i in range(1,101)][::-1],key='_PLACEMENT_')],
     [sg.Text('Summarize your death.', size=(21,1)), sg.InputText(key='_DEATH_SUMMARY_')],
     [sg.Text('How can you prevent this?', size=(21,1)), sg.InputText(key='_PREVENTION_')],
     [sg.Button('Submit'), sg.Button('ResetDB'), sg.Button('Cancel')]
